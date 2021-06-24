ba717d53a6da894a60f630d518ab198b80f4633b
201603102 <2978014730101@ingenieria.usac.edu.gt>
2021-06-24 11:12:55 -0600

    Se agregaron los archivos

---
+++ index.js
@@ -0,0 +1,30 @@
+PORT = 4000
+HOST = 'localhost'
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    //res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    //res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    //res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send('53');
+})
+
+app.listen(PORT, HOST, ()=>{
+    console.log(`Escuchando puerto: http://${HOST}:${PORT}`)
+})
